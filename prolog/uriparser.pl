905261c57ff81071f066fe919f97175022e1cd93
Wouter Beek <wouter@triply.cc>
2018-07-24 16:23:35 +0200

    ADDED: resolve_uri/3.

--- prolog/uriparser.pl
+++ prolog/uriparser.pl
@@ -1,7 +1,8 @@
 :- module(
   uriparser,
   [
-    is_uri/1 % @Term
+    is_uri/1,     % @Term
+    resolve_uri/3 % +Base, +Relative, ?Absolute
   ]
 ).
 
@@ -13,5 +14,19 @@
 
 :- use_foreign_library(foreign(uriparser)).
 
+
+
+
+
+%! is_uri(@Term) is semidet.
+
 is_uri(Term) :-
   is_uri_(Term).
+
+
+
+%! resolve_uri(+Base:atom, +Relative:atom, +Absolute:atom) is semidet.
+%! resolve_uri(+Base:atom, +Relative:atom, -Absolute:atom) is det.
+
+resolve_uri(Base, Relative, Absolute) :-
+  resolve_uri_(Base, Relative, Absolute).
